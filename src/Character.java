1a8aba6257da6886eddfb033ac20a7edde80046e
adria <adriaProgrammer@gmail.com>
2022-05-21 13:24:03 +0200

    First commit and push.

---
+++ src/Character.java
@@ -0,0 +1,28 @@
+
+public class Character {
+	
+	private String name;
+	
+	private double weight;
+	
+	private double height;
+	
+	private double painTolerance;
+	
+	private double internalDamage;
+	
+	private double regeneration;
+	
+	private int level;
+	
+	private int strong;
+	
+	private int hability;
+	
+	private int magic;
+	
+	//private runSound;
+	
+	//private hurtSound;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		main.game();
+
+	}
+	
+	private void game() {
+		
+		
+		
+	}
+
+}
