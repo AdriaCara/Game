f1a1fbbc3dc2058e8e21c04987c285ea2e35787b
AdriaCara <adriaProgrammer@gmail.com>
2022-06-03 22:49:04 +0200

    Changed the Game class.

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,48 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
 
 public class Game {
 
+	Game game = new Game();
+	private double HEIGHT;
+	private double WIDTH;
+	
 	public void Start() {
+		
+		WIDTH = (Toolkit.getDefaultToolkit().getScreenSize().width / 2);
+		HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height - (Toolkit.getDefaultToolkit().getScreenSize().height / 10);
+
+		JFrame gameFrame = new JFrame("Game");
+		gameFrame.add(game);
+		gameFrame.setBounds((int)WIDTH, (int)HEIGHT, (int)WIDTH, (int)HEIGHT);
+		gameFrame.setForeground(Color.MAGENTA);
+		gameFrame.setBackground(Color.BLACK);
+		gameFrame.setResizable(false);
+		gameFrame.setLocationRelativeTo(null);
+		gameFrame.setVisible(true);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		while (true) {
+				
+				game.move();
+				game.repaint();
+				Thread.sleep(10);
+
+		}
+		
+	}
+	
+	private void paint(Graphics g) {
+		
+		super.paint(g);
+		
+		
+	}
+	
+	private void move() {
 		
 		
 		
